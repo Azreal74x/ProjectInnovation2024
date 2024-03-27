40ea23a8c64316157a1372d32f324ee0b3f1dcb6
morqp <mara.cosneanu@yahoo.com>
2024-03-27 02:15:58 +0100

    tested

--- ProjectInnovation2024/Assets/NoGlitch/Scripts/KeyUDP.cs
+++ ProjectInnovation2024/Assets/NoGlitch/Scripts/KeyUDP.cs
@@ -6,163 +6,195 @@ using UnityEngine.SceneManagement;
 using JetBrains.Annotations;
 using FMODUnity; // Import the FMODUnity namespace
 
-public class KeyUDP : MonoBehaviour {
+public class KeyUDP : MonoBehaviour
+{
 
-  [SerializeField] private GameObject senderListener;
-  private PcListener pcListener; //cache component
-  private PcSender pcSender; //cache component
+    [SerializeField] private GameObject senderListener;
+    private PcListener pcListener; //cache component
+    private PcSender pcSender; //cache component
 
-  [SerializeField] private string keyItem = "Key"; //has to be name of the item in the inventory
+    [SerializeField] private string keyItem = "Key"; //has to be name of the item in the inventory
 
-  [SerializeField] private GameObject calibrationController;
-  private Calibration calibration;
+    [SerializeField] private GameObject calibrationController;
+    private Calibration calibration;
 
-  [SerializeField] private bool easing = false;
-  [SerializeField] private float rotationSpeed = 2f;
-  private Quaternion targetRotation;
-  private Vector3 spriteRotation;
+    [SerializeField] private bool easing = false;
+    [SerializeField] private float rotationSpeed = 2f;
+    private Quaternion targetRotation;
+    private Vector3 spriteRotation;
 
-  [SerializeField] private float minPhoneRotationX;
-  [SerializeField] private float maxPhoneRotationX;
-  [SerializeField] private float minTurnAngleZ;
-  [SerializeField] private float maxTurnAngleZ;
+    [SerializeField] private float minPhoneRotationX;
+    [SerializeField] private float maxPhoneRotationX;
+    [SerializeField] private float minTurnAngleZ;
+    [SerializeField] private float maxTurnAngleZ;
 
-  private bool key = false;
-  private bool didBoom = false;
+    private bool key = false;
+    private bool didBoom = false;
 
-  [SerializeField] private GameObject doorEnd;
+    [SerializeField] private GameObject doorEnd;
 
-  public bool gameEnd = false;
+    public bool gameEnd = false;
 
-  [SerializeField] private GameObject endComic;
-
-  private FMOD.Studio.EventInstance pickupSound;
-  [FMODUnity.EventRef][SerializeField] private string fmodPickupSound;
-
-  private FMOD.Studio.EventInstance endMusic;
-  [FMODUnity.EventRef][SerializeField] private string fmodEndMusic;
-
-  private FMOD.Studio.EventInstance openMetalDoor;
-  [FMODUnity.EventRef][SerializeField] private string fmodOpenMetalDoor;
+    [SerializeField] private GameObject endComic;
 
 
-  private void Start() {
-    if (SystemInfo.supportsGyroscope) { //check if device has gyroscope
-      Input.gyro.enabled = true; //enable use of gyroscope
-    }
-    else {
-      //Debug.Log("Gyroscope not supported"); //message if not supported
-    }
+    private FMOD.Studio.EventInstance pickupSound;
+    [FMODUnity.EventRef][SerializeField] private string fmodPickupSound;
 
-    if (senderListener != null) {
-      pcListener = senderListener.GetComponent<PcListener>();
-      pcSender = senderListener.GetComponent<PcSender>();
-    }
+    private FMOD.Studio.EventInstance endMusic;
+    [FMODUnity.EventRef][SerializeField] private string fmodEndMusic;
 
-    if (calibrationController != null) {
-      calibration = calibrationController.GetComponent<Calibration>();
-    }
-
-    if (easing) {
-      targetRotation = transform.rotation;
-    }
-
-    pickupSound = FMODUnity.RuntimeManager.CreateInstance(fmodPickupSound);
-    endMusic = FMODUnity.RuntimeManager.CreateInstance(fmodEndMusic);
-    openMetalDoor = FMODUnity.RuntimeManager.CreateInstance(fmodOpenMetalDoor);
-  }
+    private FMOD.Studio.EventInstance openMetalDoor;
+    [FMODUnity.EventRef][SerializeField] private string fmodOpenMetalDoor;
 
 
-  private void Update() {
-    if (!calibration.isCalibrated) {
-      return;
-    }
-    CheckPhone();
+    private void Start()
+    {
+        if (SystemInfo.supportsGyroscope)
+        { //check if device has gyroscope
+            Input.gyro.enabled = true; //enable use of gyroscope
+        }
+        else
+        {
+            //Debug.Log("Gyroscope not supported"); //message if not supported
+        }
 
-    //Debug.Log("key? " + key);
+        if (senderListener != null)
+        {
+            pcListener = senderListener.GetComponent<PcListener>();
+            pcSender = senderListener.GetComponent<PcSender>();
+        }
 
-    if (key && pcListener.currentItem == "Key") {
-      GyroCheck();
-    }
-  }
+        if (calibrationController != null)
+        {
+            calibration = calibrationController.GetComponent<Calibration>();
+        }
 
-  private void GyroCheck() {
-    Quaternion currentGyroData = pcListener.gyroQuaternion;
-    Quaternion correctedOrientation = currentGyroData * calibration.initialOrientation;
-    Vector3 gyroRot = correctedOrientation.eulerAngles; // Use corrected orientation
+        if (easing)
+        {
+            targetRotation = transform.rotation;
+        }
 
-    if (gyroRot.x > minPhoneRotationX && gyroRot.x < maxPhoneRotationX) { //check we are pouring within the phone upright position within the x range
-      spriteRotation = new Vector3(0, 0, -gyroRot.z); //due to weird coordinate space we set the spriteRotation's Z to -y
-
-      if (gyroRot.z > minTurnAngleZ && gyroRot.z < maxTurnAngleZ) { //check if we are pouring correct direction
-
-        OpenDoor();
-      }
+        pickupSound = FMODUnity.RuntimeManager.CreateInstance(fmodPickupSound);
+        endMusic = FMODUnity.RuntimeManager.CreateInstance(fmodEndMusic);
+        openMetalDoor = FMODUnity.RuntimeManager.CreateInstance(fmodOpenMetalDoor);
     }
 
-    if (easing) {
-      targetRotation = Quaternion.Euler(spriteRotation); //easing
-      transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * rotationSpeed);
+
+    private void Update()
+    {
+        if (!calibration.isCalibrated)
+        {
+            return;
+        }
+        CheckPhone();
+
+        //Debug.Log("key? " + key);
+
+        if (key && pcListener.currentItem == "Key")
+        {
+            GyroCheck();
+        }
     }
-    else {
-      transform.rotation = Quaternion.Euler(spriteRotation); //set the current sprite rotation to the vector with Euler to avoid gimball lock
+
+    private void GyroCheck()
+    {
+        Quaternion currentGyroData = pcListener.gyroQuaternion;
+        Quaternion correctedOrientation = currentGyroData * calibration.initialOrientation;
+        Vector3 gyroRot = correctedOrientation.eulerAngles; // Use corrected orientation
+
+        if (gyroRot.x > minPhoneRotationX && gyroRot.x < maxPhoneRotationX)
+        { //check we are pouring within the phone upright position within the x range
+            spriteRotation = new Vector3(0, 0, -gyroRot.z); //due to weird coordinate space we set the spriteRotation's Z to -y
+
+            if (gyroRot.z > minTurnAngleZ && gyroRot.z < maxTurnAngleZ)
+            { //check if we are pouring correct direction
+
+                OpenDoor();
+            }
+        }
+
+        /*if (easing)
+        {
+            targetRotation = Quaternion.Euler(spriteRotation); //easing
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * rotationSpeed);
+        }
+        else
+        {
+            transform.rotation = Quaternion.Euler(spriteRotation); //set the current sprite rotation to the vector with Euler to avoid gimball lock
+        }*/
+
+        //Debug.Log(gyroRot); //show text
     }
 
-    //Debug.Log(gyroRot); //show text
-  }
+    private void OpenDoor()
+    {
+        if (!didBoom)
+        {
+            //Debug.Log("Boom");
 
-  private void OpenDoor() {
-    if (!didBoom) {
-      //Debug.Log("Boom");
+            doorEnd.SetActive(true);
 
-      doorEnd.SetActive(true);
+            pcSender.SendUsedItem(keyItem);
 
-      pcSender.SendUsedItem(keyItem);
+            gameEnd = true;
+            StartCoroutine(WaitSomeSec());
 
-      gameEnd = true;
-      StartCoroutine(WaitSomeSecs());
+            openMetalDoor.start();
+        }
+        didBoom = true;
 
-      openMetalDoor.start();
     }
-    didBoom = true;
-
-  }
 
 
-  private IEnumerator WaitSomeSecs() {
-    yield return new WaitForSeconds(2);
+    private IEnumerator WaitSomeSec()
+    {
+        yield return new WaitForSeconds(2);
 
-    endComic.SetActive(true);
-    endMusic.start(); //THESE WILL PLAY AND LOOP EVERY FRAME. WHEN YOU HAVE A WAY TO DETECT THE GAME HAS STARTED (when we play the normal music) BE SURE TO PUTT IN A
+        endComic.SetActive(true);
+        endMusic.start();   //THESE WILL PLAY AND LOOP EVERY FRAME. WHEN YOU HAVE A WAY TO DETECT THE GAME HAS STARTED (when we play the normal music) BE SURE TO PUTT IN A
 
+        Debug.Log("waited 2 secs");
+        StartCoroutine(WaitMoreSec());
 
-    yield return new WaitForSeconds(7);
-
-    SceneManager.LoadScene(0);
-    endMusic.release();
-
-  }
-
-
-  private void CheckPhone() {
-    if (calibration.iphone) {
-      minPhoneRotationX = 270f;
-      maxPhoneRotationX = 310f;
-      minTurnAngleZ = 190f;
-      maxTurnAngleZ = 200f;
     }
-    else {
-      minPhoneRotationX = 50f;
-      maxPhoneRotationX = 90f;
-      minTurnAngleZ = 170f;
-      maxTurnAngleZ = 180f;
-    }
-  }
 
-  public void KeyPicked() {
-    key = true;
-    pcSender.SendItem(keyItem);
-    pickupSound.start();
-  }
+    private IEnumerator WaitMoreSec()
+    {
+        yield return new WaitForSeconds(5);
+
+        Debug.Log("waited 5 sec");
+
+        endMusic.release();
+
+        SceneManager.LoadScene(0);
+    }
+
+
+    private void CheckPhone()
+    {
+        if (calibration.iphone)
+        {
+            minPhoneRotationX = 270f;
+            maxPhoneRotationX = 310f;
+            minTurnAngleZ = 190f;
+            maxTurnAngleZ = 200f;
+        }
+        else
+        {
+            minPhoneRotationX = 50f;
+            maxPhoneRotationX = 90f;
+            minTurnAngleZ = 170f;
+            maxTurnAngleZ = 180f;
+        }
+    }
+
+    public void KeyPicked()
+    {
+        key = true;
+        pcSender.SendItem(keyItem);
+        pickupSound.start();
+
+    }
 
 }
--- ProjectInnovation2024/Assets/Scenes/BetaScene/MechanicsManager.cs
+++ ProjectInnovation2024/Assets/Scenes/BetaScene/MechanicsManager.cs
@@ -8,199 +8,224 @@ using FMODUnity; // Import the FMODUnity namespace
 
 
 
-public class MechanicsManager : MonoBehaviour {
-  [SerializeField] private GameObject senderListener;
-  private PcSender pcSender; //cache component
+public class MechanicsManager : MonoBehaviour
+{
+    [SerializeField] private GameObject senderListener;
+    private PcSender pcSender; //cache component
 
-  [SerializeField] private string waterItem = "Water"; //has to be name of the item in the inventory
-  [SerializeField] private string uvLightItem = "UVLight"; //has to be name of the item in the inventory
-  [SerializeField] private string noteItem = "Note"; //has to be name of the item in the inventory
+    [SerializeField] private string waterItem = "Water"; //has to be name of the item in the inventory
+    [SerializeField] private string uvLightItem = "UVLight"; //has to be name of the item in the inventory
+    [SerializeField] private string noteItem = "Note"; //has to be name of the item in the inventory
 
-  [SerializeField] private GameObject backgroundSwitchingController;
-  private BackgroundSwitching backgroundSwitching;
+    [SerializeField] private GameObject backgroundSwitchingController;
+    private BackgroundSwitching backgroundSwitching;
 
-  [SerializeField] private List<GameObject> pickups;
+    [SerializeField] private List<GameObject> pickups;
 
-  private bool interactedWithUV = false;
-  private bool sulfur = false;
+    private bool interactedWithUV = false;
+    private bool sulfur = false;
 
-  [SerializeField] private GameObject arrowLeft;
-  [SerializeField] private GameObject arrowRight;
+    [SerializeField] private GameObject arrowLeft;
+    [SerializeField] private GameObject arrowRight;
 
-  [SerializeField] private GameObject potionToPour;
-
-
-  [SerializeField] private GameObject roomBrewery;
-  [SerializeField] private GameObject roomKey;
-  [SerializeField] private GameObject roomDoor;
-  [SerializeField] private GameObject endScreen;
+    [SerializeField] private GameObject potionToPour;
 
 
-  private FMOD.Studio.EventInstance pickupSound;
-  [FMODUnity.EventRef][SerializeField] private string fmodPickupSound; // The name of the FMOD event you want to trigger
+    [SerializeField] private GameObject roomBrewery;
+    [SerializeField] private GameObject roomKey;
+    [SerializeField] private GameObject roomDoor;
+    [SerializeField] private GameObject endScreen;
 
-  private FMOD.Studio.EventInstance notePickupSound;
-  [FMODUnity.EventRef][SerializeField] private string fmodNotePickupSound;
+    [SerializeField] private GameObject calibrationObj;
 
 
-  private FMOD.Studio.EventInstance backgroundSound;
-  [FMODUnity.EventRef][SerializeField] private string fmodBackgroundSound;
+    private FMOD.Studio.EventInstance pickupSound;
+    [FMODUnity.EventRef][SerializeField] private string fmodPickupSound; // The name of the FMOD event you want to trigger
 
-  private void Start() {
-    if (senderListener != null) {
-      pcSender = senderListener.GetComponent<PcSender>();
+    private FMOD.Studio.EventInstance notePickupSound;
+    [FMODUnity.EventRef][SerializeField] private string fmodNotePickupSound;
+
+
+    private FMOD.Studio.EventInstance backgroundSound;
+    [FMODUnity.EventRef][SerializeField] private string fmodBackgroundSound;
+
+    private void Start()
+    {
+        if (senderListener != null)
+        {
+            pcSender = senderListener.GetComponent<PcSender>();
+        }
+
+        backgroundSwitching = backgroundSwitchingController.GetComponent<BackgroundSwitching>();
+
+        pickupSound = FMODUnity.RuntimeManager.CreateInstance(fmodPickupSound); // Create an instance of the FMOD event made by sno :D (i don't do CamelCasing)
+        notePickupSound = FMODUnity.RuntimeManager.CreateInstance(fmodNotePickupSound);
+        backgroundSound = FMODUnity.RuntimeManager.CreateInstance(fmodBackgroundSound);
+
+        backgroundSound.start();
     }
 
-    backgroundSwitching = backgroundSwitchingController.GetComponent<BackgroundSwitching>();
-
-    pickupSound = FMODUnity.RuntimeManager.CreateInstance(fmodPickupSound); // Create an instance of the FMOD event made by sno :D (i don't do CamelCasing)
-    notePickupSound = FMODUnity.RuntimeManager.CreateInstance(fmodNotePickupSound);
-    backgroundSound = FMODUnity.RuntimeManager.CreateInstance(fmodBackgroundSound);
-
-    backgroundSound.start();
-  }
-
-  private void Update() {
+    private void Update()
+    {
 
 
-    //Debug.Log(currentScreen);
+        //Debug.Log(currentScreen);
 
-    //Debug.Log("hammer? " + pickups[1].GetComponentInChildren<HammerUDP>().hammer);
+        //Debug.Log("hammer? " + pickups[1].GetComponentInChildren<HammerUDP>().hammer);
 
-    SwitchMechanics(backgroundSwitching.currentScreen);
+        SwitchMechanics(backgroundSwitching.currentScreen);
 
-  }
-  private void SwitchMechanics(int room) {
+    }
+    private void SwitchMechanics(int room)
+    {
 
-    /*if (pickups[0].GetComponentInChildren<UVLightUDP>().discovered)
+        /*if (pickups[0].GetComponentInChildren<UVLightUDP>().discovered)
+        {
+            interactedWithUV = true;
+        }
+
+
+
+        else if (room != 0)
+        {
+            pickups[0].SetActive(false);
+        }
+
+        if (room != 0)
+        {
+            pickups[0].SetActive(false);
+        }
+
+        ////////////
+        if (room == 1)  
+        {
+           */
+        if (room == 0)
+        {
+            pickups[0].SetActive(true); // 
+            /* if (pickups[3].GetComponent<MixUDP>().check) //activate throw
+             {
+                 pickups[4].SetActive(true);
+             }
+             if (pickups[4].GetComponentInChildren<ThrowUDP>().explosion) // after thrown activate door
+             {
+                 pickups[5].SetActive(true);
+             }*/
+        }
+        else if (room != 0)
+        {
+            pickups[0].SetActive(false);
+        }
+        ////////////
+        if (room == 1)
+        {
+            pickups[1].SetActive(true);
+
+        }
+        else if (room != 1)
+        {
+            pickups[1].SetActive(false);
+
+        }
+        ////////////
+        if (room == 2)  //  room 2  pour & mix potion
+        {
+            pickups[2].SetActive(true);
+
+        }
+        else if (room != 2)
+        {
+            pickups[2].SetActive(false);
+            //pickups[3].SetActive(false);
+        }
+
+        ////////////
+
+        if (roomBrewery.activeSelf)  //      zoom in room 2 
+        {
+            pickups[2].SetActive(false);
+
+            pickups[3].SetActive(true);
+
+            if (sulfur) // if we took the potion
+            {
+                potionToPour.SetActive(true); // show the vessel wuth mix in it
+            }
+        }
+        else if (!roomBrewery.activeSelf)
+        {
+            //pickups[6].SetActive(false);
+
+            //pickups[2].SetActive(false);
+
+            pickups[3].SetActive(false);
+        }
+        //////////////
+        if (roomKey.activeSelf)  //      zoom in room 2 
+        {
+            pickups[0].SetActive(false);
+
+            pickups[4].SetActive(true);
+        }
+        else if (!roomBrewery.activeSelf)
+        {
+            pickups[4].SetActive(false);
+        }
+
+        if (roomDoor.activeSelf)
+        {
+            pickups[2].SetActive(false);
+
+            pickups[5].SetActive(true);
+        }
+        else if (!roomBrewery.activeSelf)
+        {
+            pickups[5].SetActive(false);
+        }
+
+
+        if (endScreen.activeSelf || calibrationObj.activeSelf)
+        {
+            backgroundSound.release();
+            for (int i = 0; i < pickups.Count; i++)
+            {
+                pickups[i].SetActive(false);
+            }
+        }
+
+        
+    }
+
+    public void InteractedWithUV()
     {
         interactedWithUV = true;
     }
 
-
-
-    else if (room != 0)
+    public void GotSulfur()
     {
-        pickups[0].SetActive(false);
+        sulfur = true;
+
+        pickupSound.start(); // Start playing the FMOD event
     }
 
-    if (room != 0)
+    public void GotWater()
     {
-        pickups[0].SetActive(false);
+        pcSender.SendItem(waterItem);
+
+        pickupSound.start(); // Start playing the FMOD event
     }
 
-    ////////////
-    if (room == 1)  
+    public void GotUVFlashlight()
     {
-       */
-    if (room == 0) {
-      pickups[0].SetActive(true); // 
-      /* if (pickups[3].GetComponent<MixUDP>().check) //activate throw
-       {
-           pickups[4].SetActive(true);
-       }
-       if (pickups[4].GetComponentInChildren<ThrowUDP>().explosion) // after thrown activate door
-       {
-           pickups[5].SetActive(true);
-       }*/
-    }
-    else if (room != 0) {
-      pickups[0].SetActive(false);
-    }
-    ////////////
-    if (room == 1) {
-      pickups[1].SetActive(true);
+        pcSender.SendItem(uvLightItem);
 
+        pickupSound.start(); // Start playing the FMOD event
     }
-    else if (room != 1) {
-      pickups[1].SetActive(false);
 
-    }
-    ////////////
-    if (room == 2)  //  room 2  pour & mix potion
+    public void GotNote()
     {
-      pickups[2].SetActive(true);
+        pcSender.SendItem(noteItem);
 
+        notePickupSound.start(); // Start playing the FMOD event
     }
-    else if (room != 2) {
-      pickups[2].SetActive(false);
-      //pickups[3].SetActive(false);
-    }
-
-    ////////////
-
-    if (roomBrewery.activeSelf)  //      zoom in room 2 
-    {
-      pickups[2].SetActive(false);
-
-      pickups[3].SetActive(true);
-
-      if (sulfur) // if we took the potion
-      {
-        potionToPour.SetActive(true); // show the vessel wuth mix in it
-      }
-    }
-    else if (!roomBrewery.activeSelf) {
-      //pickups[6].SetActive(false);
-
-      //pickups[2].SetActive(false);
-
-      pickups[3].SetActive(false);
-    }
-    //////////////
-    if (roomKey.activeSelf)  //      zoom in room 2 
-    {
-      pickups[0].SetActive(false);
-
-      pickups[4].SetActive(true);
-    }
-    else if (!roomBrewery.activeSelf) {
-      pickups[4].SetActive(false);
-    }
-
-    if (roomDoor.activeSelf) {
-      pickups[2].SetActive(false);
-
-      pickups[5].SetActive(true);
-    }
-    else if (!roomBrewery.activeSelf) {
-      pickups[5].SetActive(false);
-    }
-
-
-    if (endScreen.activeSelf) {
-      backgroundSound.release();
-      for (int i = 0; i < pickups.Count; i++) {
-        pickups[i].SetActive(false);
-      }
-    }
-  }
-
-  public void InteractedWithUV() {
-    interactedWithUV = true;
-  }
-
-  public void GotSulfur() {
-    sulfur = true;
-
-    pickupSound.start(); // Start playing the FMOD event
-  }
-
-  public void GotWater() {
-    pcSender.SendItem(waterItem);
-
-    pickupSound.start(); // Start playing the FMOD event
-  }
-
-  public void GotUVFlashlight() {
-    pcSender.SendItem(uvLightItem);
-
-    pickupSound.start(); // Start playing the FMOD event
-  }
-
-  public void GotNote() {
-    pcSender.SendItem(noteItem);
-
-    notePickupSound.start(); // Start playing the FMOD event
-  }
 }
